597a2588068e3f3dbe0da9c1faf8ea7289d7bc1e
Rezvan Orlechuk <rorlechuk@smustafin.(none)>
2012-03-22 18:05:56 +0400

    wiki. Abstract

---
+++ wiki.py
@@ -0,0 +1,28 @@
+'''
+Created on Mar 22, 2012
+
+@author: Rezvan aka DreamBit aka John_Pa9JIbHuK
+
+@organization: GridDynamics
+'''
+
+class WikiConnector:
+    '''
+    Class for creating connection to wiki and working with XMLRPC
+    '''
+    def __init__(self, wikiURL, wikiLogin, wikiPassword, mainSpace):
+        '''
+        Constructor
+        '''
+        self.wikiURL = wikiURL
+        self.wikiLogin = wikiLogin
+        self.wikiPassword = wikiPassword
+        pass
+    
+    def createDesignContent(self, content):
+        pass
+    
+    
+    def sendDesignContent(self, designContent):
+        pass
+    
